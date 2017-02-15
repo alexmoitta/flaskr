253a24bc84d1724fd57f08001f36bdd12bf13d9d
alexmoitta <alex@geezer.local>
2017-02-15 06:33:30 -0200

    Recursion call almost right.

--- progAssignement3/quicksort.py
+++ progAssignement3/quicksort.py
@@ -6,12 +6,12 @@ from numpy import array
 
 
 
-def partition(array_numbers,pivot,r):
+def partition(array_numbers,pivot,r, number_comparisons):
 #falta colocar o caso base de um elemento ainda e a recursao
 #   r = len(array_numbers)
     i = pivot + 1
     if (r - i) < 1:
-        return 1
+        return number_comparisons+0 #no comparison.
     tmp_swap = 0
     for j in range(pivot+1,r,1):
         if array_numbers[j] < array_numbers[pivot]:
@@ -22,15 +22,20 @@ def partition(array_numbers,pivot,r):
     tmp_swap = array_numbers[pivot]
     array_numbers[pivot] = array_numbers[i-1]
     array_numbers[i-1] = tmp_swap
-    partition(array_numbers,i,r) #chamada para a direita
-#    partition(array_numbers,0,i-1) #chamada para a esquerda
+    number_comparisons += partition(array_numbers,pivot,i,number_comparisons)
+    if (r - i <= 1 ):
+        return number_comparisons + (r - 1)
+    else:
+        number_comparisons += partition(array_numbers,i,r,number_comparisons) #chamada para a direita
     print array_numbers
-    return i-1
+    return number_comparisons+(r-pivot-1) #this should include the numbers of this recursion
 
-def quicksort_by_me(array_numbers,pivot,r):
+def quicksort_by_me(array_numbers,pivot,r,number_comparisons):
     r = len(array_num_input)
     while r > 1:
-        r = partition(array_num_input,0,r)
+        r = partition(array_num_input,0,r,number_comparisons)
+    print "quantidade de inversoes: " + str(r)
+
 
 workdirectory = "./workdir"
 name_of_file = "testePequeno.txt"
@@ -56,7 +61,8 @@ array_num_input = array(number_list, dtype = 'L')
 
 print "File loading ended."
 
-quicksort_by_me(array_num_input,0,len(array_num_input))
+number_comparisons = 0
+quicksort_by_me(array_num_input,0,len(array_num_input),number_comparisons)
 
 file_pointer = open(outputnameofile,mode='w')
 
