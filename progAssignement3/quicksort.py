02579344fabb71ecd4253d775c63ea5fbbc88e53
alexmoitta <alex@geezer.local>
2017-02-19 06:32:21 -0300

    Prog Assignement works.

--- progAssignement3/quicksort.py
+++ progAssignement3/quicksort.py
@@ -52,11 +52,77 @@ def quicksort_by_the_book_last(array_num_input_last, p, r, number_comparisons_la
         quicksort_by_the_book_last(array_num_input_last,q,r,number_comparisons_last)
 
 
+# se vier impar, p ex 5, 5 / 2 = 2 de 0th, 1th, 2th, 3th, 4th
+# se vier par, p ex 4, 4 / 2 = 2 ele pede que seja o 1th e nao 2th
+def swap_first_median(array_num_input_median,p,r):
+    if (r - p) % 2 == 0: #nro elementos pares
+#        pos_first_median = (r-1-p)/2
+        pos_first_median = (r-1-p)/2+p
+        first_median = array_num_input_median[pos_first_median]
+    else:
+#        pos_first_median = (r-p)/2
+        pos_first_median = (r-p)/2+p
+        first_median = array_num_input_median[pos_first_median]
+
+    #median selecionado para comparar com o ultimo e o primeiro
+    first_element = array_num_input_median[p]
+    last_element = array_num_input_median[r-1]
+
+    if first_median <= last_element:
+        if first_median >= first_element:
+            real_median = first_median
+            #iniciando o swap com o primeiro elemento
+            tmp_swap = array_num_input_median[p]
+            array_num_input_median[p] = real_median
+            array_num_input_median[pos_first_median] = tmp_swap
+            return
+    if first_median >= last_element:
+        if first_median <= first_element:
+            real_median = first_median
+            #iniciando o swap com o primeiro elemento
+            tmp_swap = array_num_input_median[p]
+            array_num_input_median[p] = real_median
+            array_num_input_median[pos_first_median] = tmp_swap
+            return
+
+    if last_element <= first_median:
+        if last_element >= first_element:
+            real_median = last_element
+            tmp_swap = array_num_input_median[p]
+            array_num_input_median[p] = real_median
+            array_num_input_median[r-1] = tmp_swap
+            return
+    if last_element >= first_median:
+        if last_element <= first_element:
+            real_median = last_element
+            tmp_swap = array_num_input_median[p]
+            array_num_input_median[p] = real_median
+            array_num_input_median[r-1] = tmp_swap
+            return
+    #else
+    #continua do jeito que esta porque o primeiro elemento ja deve ser a mediana
+
+
+
+
+def quicksort_by_the_book_median(array_num_input_median, p, r, number_comparisons_median):
+    if p < r:
+        swap_first_median(array_num_input_median,p,r)
+        q = partition(array_num_input_median,p,r,number_comparisons_median)
+
+        #        swap_first_last(array_numbers,p,q-1)
+        quicksort_by_the_book_median(array_num_input_median,p,q-1,number_comparisons_median)
+
+        #        swap_first_last(array_numbers,q,r)
+        quicksort_by_the_book_median(array_num_input_median,q,r,number_comparisons_median)
 
 
 
 
 workdirectory = "./workdir"
+#name_of_file = "10.txt"
+#name_of_file = "100.txt"
+#name_of_file = "1000.txt"
 #name_of_file = "testePequeno.txt"
 name_of_file = "_32387ba40b36359a38625cbb397eee65_QuickSort.txt"
 numbers_file = workdirectory + "/" + name_of_file
@@ -78,14 +144,18 @@ file_pointer.close()
 
 array_num_input = array(number_list, dtype = 'L')
 array_num_input_last = array(number_list, dtype = 'L')
+array_num_input_median = array(number_list, dtype = 'L')
+
 
 print "File loading ended."
 
 number_comparisons = ComparisonCounter.ComparisonCounter()
 number_comparisons_last = ComparisonCounter.ComparisonCounter()
+number_comparisons_median = ComparisonCounter.ComparisonCounter()
 
-quicksort_by_the_book(array_num_input,0, len(array_num_input), number_comparisons)
-quicksort_by_the_book_last(array_num_input_last,0, len(array_num_input_last), number_comparisons_last)
+#quicksort_by_the_book(array_num_input,0, len(array_num_input), number_comparisons)
+#quicksort_by_the_book_last(array_num_input_last,0, len(array_num_input_last), number_comparisons_last)
+quicksort_by_the_book_median(array_num_input_median,0,len(array_num_input_median),number_comparisons_median)
 
 
 file_pointer = open(outputnameofile,mode='w')
